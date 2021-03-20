b9f28f4f0b5970fdb3d1b2b055a18bbb2af4a45d
Marc Robin Gruener <gmarcrobin@outlook.de>
2021-03-20 12:07:59 +0100

    leaderboard

--- flask/app.py
+++ flask/app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
+leaderboard_header = ('Rank','Icon','Name','Points')
+leaderboard_data = (
+    (1,'', 'Andri Turra', 173),
+    (2,'', 'Armando', 165),
+    (3,'', 'Marc', 147)
+)
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
@@ -9,7 +15,7 @@ def home():
 
 @app.route('/leaderboard', methods=['POST', 'GET'])
 def leaderboard():
-    return render_template('leaderboard.html')
+    return render_template('leaderboard.html', leaderboard_header=leaderboard_header, leaderboard_data=leaderboard_data)
 
 @app.route('/challenges', methods=['POST', 'GET'])
 def recommendations():
@@ -21,6 +27,6 @@ def about():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
 
