4e9ef38cec43f34c9f1d7da774264bc28f1da266
Marc Robin Gruener <gmarcrobin@outlook.de>
2021-03-20 11:49:05 +0100

    Pictures

--- Tracker_Data/Tracker_data_activity_day_summary.py
+++ Tracker_Data/Tracker_data_activity_day_summary.py
@@ -1,14 +1,16 @@
-# import pandas as pd
+import pandas as pd
 import os
 
 file_path = '/Users/marcgruner/Downloads/samsunghealth_nevzad.nuhiu_202103172116/jsons/com.samsung.shealth.activity.day_summary'
 
-# df_activity_day_summary = pd.DataFame()
+df_activity_day_summary = pd.DataFame()
 
 for sub_dir in os.listdir(file_path):
     print(sub_dir)
     if sub_dir == '.DS_Store': continue
     for filename in os.listdir(file_path+'/'+sub_dir):
         print(filename)
-        # temp_df = pd.read_json(filename)
-        # df_activity_day_summary.append(temp_df)
+        temp_df = pd.read_json(filename)
+        df_activity_day_summary.append(temp_df)
+
+print(df_activity_day_summary)
